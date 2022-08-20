aa7296fe8238bbeb75529cfc56367c62eb4b665e
Cofucan <coolucheofia@gmail.com>
2022-08-20 17:20:01 +0100

    style: Apply code formatting and add style guides

--- favigen/models.py
+++ favigen/models.py
@@ -1,9 +1,6 @@
-from ast import Try
-from tabnanny import verbose
-from django.db import models
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from .managers import CustomUserManager
